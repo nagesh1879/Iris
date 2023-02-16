d0d5d7b3f7f7f775149e0ff5a83533de021f99cd
nagesh <nagesh.s.pai@gmail.com>
2023-02-16 09:26:40 +0530

    test

--- model.py
+++ model.py
@@ -11,7 +11,7 @@ labels ={
 }
 #split the data set
 from sklearn.model_selection import train_test_split
-x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=.95:wq)
+x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=.55)
 #Using decision tree algorithm
 from sklearn import tree
 classifier=tree.DecisionTreeClassifier()
